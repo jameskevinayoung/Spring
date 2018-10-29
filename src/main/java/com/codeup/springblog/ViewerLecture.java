64ccec9d9f46e1792df8606402e4d555961e43c9
jameskevinayoung <james.kevin.a.young@gmail.com>
2018-10-29 16:23:45 -0500

    finished viewer lecture

---
+++ src/main/java/com/codeup/springblog/ViewerLecture.java
@@ -0,0 +1,26 @@
+package com.codeup.springblog;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@Controller
+public class ViewerLecture {
+    @GetMapping("/users/{role}")
+    public String showUsers(@PathVariable String role, Model viewModel){
+        List<String> users = new ArrayList<>();
+        users.add("James");
+        users.add("Kevin");
+        users.add("Alexa");
+        users.add("Young");
+
+        viewModel.addAttribute("users", users);
+        viewModel.addAttribute("role", role);
+
+        return "users";
+    }
+}
