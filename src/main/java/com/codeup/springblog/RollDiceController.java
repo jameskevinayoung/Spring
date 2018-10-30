5444b19db05801032c5cc757bfebbfeef08eaf37
jameskevinayoung <james.kevin.a.young@gmail.com>
2018-10-29 20:25:49 -0500

    created guessNumber method

--- src/main/java/com/codeup/springblog/RollDiceController.java
+++ src/main/java/com/codeup/springblog/RollDiceController.java
@@ -1,8 +1,10 @@
 package com.codeup.springblog;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.ResponseBody;
 
 import java.util.Random;
 
@@ -10,16 +12,25 @@ import java.util.Random;
 @Controller
 public class RollDiceController {
 
+    @GetMapping("/roll-dice")
+    @ResponseBody
+    public String pickNumber(){
+        return "Pick a number";
+    }
+
     @GetMapping("/roll-dice/{number}")
-    public String guessNumber(@PathVariable int number){
-        int userGuess = number;
+    public String guessNumber(@PathVariable int number, Model viewNumber){
+
         Random result = new Random();
         int value = result.nextInt(6)+1;
 
-        if(userGuess == value){
-            return "Your number "+ userGuess + " matches the random number: "+ value;
+        viewNumber.addAttribute("number",number);
+        viewNumber.addAttribute("value", value);
+
+        if(number == value){
+            return "Your number "+ number + " matches the random number: "+ value;
         }else {
-            return "Your number "+ userGuess + " didn't match the random number: "+value;
+            return "Your number "+ number + " didn't match the random number: "+value;
         }
     }
 
