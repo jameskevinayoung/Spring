699cc6368842e3cc42be1d627dc9afc86691d3f7
jameskevinayoung <james.kevin.a.young@gmail.com>
2018-10-31 11:18:09 -0500

    added notes

--- src/main/java/com/codeup/springblog/Services/PostsServices.java
+++ src/main/java/com/codeup/springblog/Services/PostsServices.java
@@ -1,44 +1,61 @@
 package com.codeup.springblog.Services;
-
-
-import com.codeup.springblog.controllers.Post;
 import org.springframework.stereotype.Service;
-
 import java.util.ArrayList;
 import java.util.List;
+
+
+
+//@Service annotation identifies the class as an intermediary between the controller and the templates to handle functionality.
 
 @Service
 public class PostsServices {
 
+    //creating a property that is a list of posts
     private List<Post> posts;
 
+    //constructor that creates an array list from the list of posts
     public PostsServices() {
         posts = new ArrayList<>();
         createPosts();
     }
 
-
-    public Post findOne(long id){
-        return posts.get((int)id-1);
+    /**
+     * this method finds a post by the id property of a Post object, takes the array list of posts and searches
+     * for the id number (that starts with 1) and subtracts 1 to get the index (which starts with 0) of the array list
+     */
+    public Post findOne(int id){
+        return posts.get(id-1);
     }
 
+
+    //this method returns all of the posts in the array list
 
     public List<Post> findAll(){
         return posts;
     }
 
+    /**
+     * this method takes a post and sets the id to 1 number greater than the size of the posts array list
+     * and adds it to the end of the current list, then returns that post.
+     *
+     */
 
-    public Post save(Post post){
+    public Post add(Post post){
         post.setId(posts.size()+1);
         posts.add(post);
         return post;
     }
 
-
+    /**
+     * this method uses the add method for each new instance of a post to be created, uses the Post constructor that takes in the
+     * the title and body parameters to construct a post. This is void because you are not specifically returning anything, just
+     * creating the information that will be pulled somewhere else to be displayed on the webpage.
+     * these posts could be added directly into the constructor instead of the createPosts method.
+     */
     private void createPosts(){
-        this.save(new Post("My First Post", "I'm sick of this shit"));
-        this.save(new Post("A good day", "to learn code"));
-        this.save(new Post("A bad day", "to learn code on an empty stomach"));
+        this.add(new Post("My First Post", "I'm sick of this shit"));
+        this.add(new Post("A good day", "to learn code"));
+        this.add(new Post("A bad day", "to learn code on an empty stomach"));
 
     }
 
--- src/main/java/com/codeup/springblog/controllers/PostController.java
+++ src/main/java/com/codeup/springblog/controllers/PostController.java
@@ -1,6 +1,4 @@
 package com.codeup.springblog.controllers;
-
-
 import com.codeup.springblog.Services.PostsServices;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
@@ -9,37 +7,34 @@ import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 
-import java.util.ArrayList;
-import java.util.List;
-
 @Controller
 public class PostController {
 
-
-    //Services property
-private PostsServices postSvc;
+//This property is the connection (intermediary) that allows the controller class to access the services class
+    private PostsServices postSvc;
 
 
 /**
- * This is constructor that takes the property as a parameter for the CONTROLLER to access the SERVICES class that will house the construction of data
- * that the CONTROLLER will pull from to render information in a logical way on the TEMPLATES
+ * This is the constructor that takes the services property as a parameter for the CONTROLLER to access the SERVICES class
+ * that will house the construction of data that the CONTROLLER will pull from to render information in a logical way
+ * on the TEMPLATES.
  *
+ * This is called Dependency Injection or passing things into the constructor of an object.
  */
 
+    public PostController(PostsServices postSvc){
+        this.postSvc = postSvc;
+    }
 
-public PostController(PostsServices postSvc){
-    this.postSvc = postSvc;
-}
-
-    /**
-     * 1) creating a list object for the posts outside of methods provides access to the list by all methods
-     * 2) creating a constructor allows the ability to construct the array list of posts
-     * 3) to render on an html page remove the @ResponseBody annotation  and add the Model object
-     * 4) use the .addAttribute() method to the Model object
-     * 5) return a pathway to the page starting inside the appropriate folder without the leading forward slash
-     *
-     * NOTE: IT IS NOT THE JOB OF THE CONTROLLER TO HOLD/CONSTRUCT THE DATA BUT TO DO THE LOGIC
-     */
+/**
+ * 1) creating a list object for the posts outside of methods provides access to the list by all methods
+ * 2) creating a constructor allows the ability to construct the array list of posts
+ * 3) to render on an html page remove the @ResponseBody annotation  and add the Model object
+ * 4) use the .addAttribute() method to the Model object
+ * 5) return a pathway to the page starting inside the appropriate folder without the leading forward slash
+ *
+ * NOTE: IT IS NOT THE JOB OF THE CONTROLLER TO HOLD/CONSTRUCT THE DATA BUT TO DO THE LOGIC
+ */
 
 
 
@@ -50,15 +45,35 @@ public PostController(PostsServices postSvc){
 //}
 
 
+/**
+ * the original structure of the postsIndex method will continue to add content to the page each time to repeat
+ * unless you create a method that allows you to add a post to an existing list and return that list each time.
+ * This will allow the functionality to update a list without it appending each time.
+ *
+ * ex: return posts; / vs / return posts + post;
+ */
 
-    @GetMapping("/posts") //this will continue to add content each time to repeat
-//    @ResponseBody
-    public String postsIndex(Model viewModel){
+
+
+    @GetMapping("/posts")//this is where the user will go to initially or type into the browser
+//    @ResponseBody //response body just renders from the controller this is not ideal when expanding the app architecture
+    public String postsIndex(Model viewModel){ //use the model object to add attributes
+
+
 //        posts.add(new Post("A good day", "to learn code"));
 //        posts.add(new Post("A bad day", "to learn code on an empty stomach"));
 
-        viewModel.addAttribute("posts", postSvc.findAll());//view model
-        return "posts/index"; //pathway
+        /**
+         * 1) take the model parameter and use addAttribute method
+         * 2) the first parameter in the addAttribute method identifies what the template will call the object (posts)
+         * 3) the second parameter identifies what the object will be; since we are using a services property for functionality
+         * 4) use findAll method, located in the services class, on the services property, which will return an array list of posts
+         */
+        viewModel.addAttribute("posts", postSvc.findAll());
+
+        //now direct the user to the specified location to view the information
+        return "posts/index"; //pathway on the server
+        // the user only sees the information rendered on the /posts but html page is protected similar to WEB-INF folder
     }
 
 
